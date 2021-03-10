1e95ed2c8ec958b1e00a9b049ec4df0de5ad933f
Jijo Mon <jijo.mon@publicissapient.com>
2021-03-10 21:55:44 +0530

    Add project files.

---
+++ devops-webapps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace devops_webapps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
